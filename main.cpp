8d427c5fb342ce9e83a6da6efc16c1a007d747be
Håkon Vågsether <hauk142@gmail.com>
2012-02-19 23:54:18 +0100

    added main.cpp

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.cpp
+ *
+ *    Description:  Jaguti main.cpp
+ *
+ *        Version:  
+ *        Created:  02/19/2012 11:52:27 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Håkon Vågsether, Audun Måseidvåg
+ *   Organization:  Jaguti
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
