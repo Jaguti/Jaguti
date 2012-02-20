221d9a0d3d47c740d143d93c53144ca00f897e77
Håkon Vågsether <hauk142@gmail.com>
2012-02-20 01:33:36 +0100

    Added a header file and some SDL initialisation

---
+++ main.h
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  main.h
+ *
+ *    Description:  Jaguti's main header file
+ *
+ *        Version:  1.0
+ *        Created:  02/20/2012 12:41:47 AM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Håkon Vågsether, Audun Måseidvåg
+ *   Organization:  Jaguti
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <iostream>
+#include <SDL/SDL.h>
+
+#define	WINDOW_HEIGHT			/* The window's height */
+#define	WINDOW_WIDTH			/* The window's width */
+#define	WINDOW_BPP			/* The window's Bits Per Pixel */
+
+int drawScreen();
