58d841bd25c9139cd7c6188fd2f842403e2d1917
Håkon Vågsether <hauk142@gmail.com>
2012-05-31 01:02:38 +0200

    Proper positioning of the square

--- main.cpp
+++ main.cpp
@@ -233,7 +233,7 @@ int drawScene()
 
 			glDisable(GL_TEXTURE_2D);
 			glColor3f(0.0f, 0.0f, 1.0f);
-			glRectf(900.0f, 15.0f, 910.0f, 25.0f);
+			glRectf(980.0f, 15.0f, 990.0f, 25.0f);
 			glColor3f(1.0f, 1.0f, 1.0f);
 			glFlush();
 
