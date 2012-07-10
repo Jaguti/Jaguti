ec689ca224d6ed31932d14e7c748fcb502195763
Håkon Vågsether <hauk142@gmail.com>
2012-07-11 01:55:14 +0200

    terribly incomplete, a small multithreaded "template" for main.cpp

--- main.cpp
+++ main.cpp
@@ -12,4 +12,25 @@
  * =====================================================================================
  */
 
+#include <iostream>
+#include <thread>
+#include <chrono>
 
+const std::chrono::duration<double> sleepDuration(0.025); /* 25 milliseconds, 1  / 40  = 0.025 			 */
+							  /* We do this in order to lock the framerate at 40 FPS */
+
+void timer1();
+
+int main()
+{
+	std::thread tTimer1(timer1);
+	std::cout << "test" << std::endl;
+	
+	tTimer1.join();
+	return 0;
+}
+ 
+void timer1()
+{
+	std::this_thread::sleep_for(sleepDuration);
+}
